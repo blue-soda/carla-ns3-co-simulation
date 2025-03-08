46d0f6cffd960fc7de578a40070ec95f29b05715
Heorhi Davydau <heorhid@gmail.com>
2025-03-08 16:26:43 +0100

    enhance vehicle management in Carla simulation; add functions for spawning multiple vehicles, setting autopilot, and destroying actors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
+import random
 
-from src.utils.carla_connector import connect_to_carla, spawn_vehicle
+from src.utils.carla_connector import connect_to_carla, spawn_vehicle, spawn_vehicles, set_autopilot, destroy_actors
 from src.models.v2x_messages import CAMGenerator, DENMGenerator
 from config.settings import CARLA_HOST, CARLA_PORT, CARLA_TIMEOUT
 
@@ -8,28 +9,37 @@ def main():
     print("Connecting to Carla simulator...")
     client, world = connect_to_carla(CARLA_HOST, CARLA_PORT, CARLA_TIMEOUT)
     
-    if not client:
+    if not client or not world:
         print("Failed to connect to Carla. Make sure the simulator is running.")
         return
     
     print("Successfully connected to Carla simulator!")
 
-    vehicle = spawn_vehicle(world, 'model3')
-
-    cam_generator = CAMGenerator(vehicle)
-    denm_generator = DENMGenerator(vehicle)
+    ego_vehicle = spawn_vehicle(world, 'model3')
+    if not ego_vehicle:
+        print("Failed to spawn ego vehicle")
+        return
+    
+    spawn_vehicles(world, 50, ['cybertruck'])
+    
+    set_autopilot(world, True)
+    
+    cam_generator = CAMGenerator(ego_vehicle)
+    denm_generator = DENMGenerator(ego_vehicle)
 
     try:
         while True:
             cam_generator.broadcast()
 
-            denm_message = denm_generator.generate_message("accident", (0, 0), "Car crash")
-            denm_generator.broadcast(denm_message)
+            # denm_message = denm_generator.generate_message("accident", (0, 0), "Car crash")
+            # denm_generator.broadcast(denm_message)
         
             time.sleep(1)
 
     except KeyboardInterrupt:
         print("\nSimulation interrupted by user")
+    finally:
+        destroy_actors(world)
     
     print("Simulation ended")
 
--- src/utils/carla_connector.py
+++ src/utils/carla_connector.py
@@ -38,13 +38,70 @@ def spawn_vehicle(world: carla.World, vehicle_type: str = None) -> Optional[carl
         blueprint_library = world.get_blueprint_library()
         
         if vehicle_type:
-            vehicle_bp = blueprint_library.filter(vehicle_type)[0]
+            vehicle_blueprint = blueprint_library.filter(vehicle_type)[0]
         else:
-            vehicle_bp = random.choice(blueprint_library.filter('vehicle'))
+            vehicle_blueprint = random.choice(blueprint_library.filter('vehicle'))
             
         spawn_point = random.choice(world.get_map().get_spawn_points())
-        vehicle = world.spawn_actor(vehicle_bp, spawn_point)
+        vehicle = world.spawn_actor(vehicle_blueprint, spawn_point)
         return vehicle
     except Exception as e:
         print(f"Error spawning vehicle: {e}")
+        return None
+
+
+def spawn_vehicles(world: carla.World, num_vehicles: int, vehicle_types: list[str] = None) -> None:
+    """
+    Spawn a number of vehicles in the world
+    
+    Args:
+        world: Carla world
+        num_vehicles: Number of vehicles to spawn
+    """
+    try:
+        blueprint_library = world.get_blueprint_library()
+
+        if vehicle_types:
+            vehicle_blueprints = [blueprint_library.filter(vehicle_type)[0] for vehicle_type in vehicle_types]
+        else:
+            vehicle_blueprints = blueprint_library.filter('*vehicle*')
+        
+        spawn_points = world.get_map().get_spawn_points()
+        
+        for i in range(num_vehicles):
+            try: 
+                world.try_spawn_actor(random.choice(vehicle_blueprints), random.choice(spawn_points))
+            except Exception as e:
+                print(f"Error spawning vehicle: {e}")
+                continue
+    except Exception as e:
+        print(f"Error spawning vehicles: {e}")
+        return None
+    
+def set_autopilot(world: carla.World, enable: bool) -> None:
+    """
+    Set autopilot mode for all vehicles in the world
+    
+    Args:
+        world: Carla world
+    """
+    try:
+        for vehicle in world.get_actors().filter('*vehicle*'):
+            vehicle.set_autopilot(enable)
+    except Exception as e:
+        print(f"Error setting autopilot: {e}")
+        return None
+
+def destroy_actors(world: carla.World) -> None:
+    """
+    Destroy all actors in the world
+    
+    Args:
+        world: Carla world
+    """
+    try:
+        for actor in world.get_actors():
+            actor.destroy()
+    except Exception as e:
+        print(f"Error destroying actors: {e}")
         return None
