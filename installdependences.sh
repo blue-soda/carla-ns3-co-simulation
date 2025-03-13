9a6b797738b610d5e66581a6b54153e37723ed28
Heorhi Davydau <heorhid@gmail.com>
2025-03-13 01:07:28 +0100

    remove outdated ns-3 installation script; add new dependency installation and ns-3 setup scripts

---
+++ installdependences.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Cheak is sudo or not
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo privileges"
+  exit
+fi
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y build-essential autoconf automake libboost-all-dev libzmq3-dev \
+                        git gcc g++ python3 python3-dev pkg-config sqlite3 \
+                        libsqlite3-dev cmake libc6-dev libc6-dev-i386 libclang-dev \
+                        nlohmann-json3-dev
+
+echo "Dependencies installation completed"
